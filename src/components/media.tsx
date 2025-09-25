71919052adc7b30fcbf0bac5eb6cd0bce5d67268
Suvan Gowri Shanker <118198968+greeenboi@users.noreply.github.com>
2025-09-25 10:11:53 +0000

    feat: add OnceMedia component for video rendering

---
+++ src/components/media.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { Media } from '@once-ui-system/core'
+import React from 'react'
+
+const OnceMedia = ({ video }: {video: {src: string, alt: string}}) => {
+  return (
+    <Media
+        src={video.src}
+        alt={video.alt}
+        aspectRatio="16/9"
+        radius="xl"
+    />
+  )
+}
+
+export default OnceMedia
